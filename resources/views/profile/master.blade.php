c462ff6167f2abfbbc4906551ccf19b52371a417
rajat98765 <rajatsinghk4@gmail.com>
2018-03-13 03:34:50 +0530

    added your preference tab

--- resources/views/profile/master.blade.php
+++ resources/views/profile/master.blade.php
@@ -22,6 +22,24 @@
 	body {
 		background-image: url('../img/bg.jpg');
 	}
+
+
+	table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+
 </style>
 </head>
 <body>
